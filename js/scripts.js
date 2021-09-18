6e175b632838a7956dc0010f66bd89d3860ede79
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-09-18 07:31:38 +0300

    Collect customer order details

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,19 @@ function PizzaOrder(size,crust,topping,quantity,delivery){
   this.quantity=quantity;
   this.delivery=delivery;
 }
+$(document).ready(function(){
+  $("form#order-form").submit(function(event){
+    event.preventDefault();
+
+    var selectedSize= parseFloat($(this).find("select.pizza-size").val());
+    var selectedCrust=parseFloat($(this).find("select.pizza-crust").val());
+    var selectedToppings=parseFloat($(this).find("select.pizza-toppings").val());
+    var inputtedQuantity=parseFloat($("input#quantity").val());
+    var customerPizzaDelivery=parseFloat($(this).find("select.pizza-delivery").val());
+
+    var newPizzaOrder=new PizzaOrder(selectedSize,selectedCrust,selectedToppings,inputtedQuantity,customerPizzaDelivery);
+
+
+
+  });
+});
