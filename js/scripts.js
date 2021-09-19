5a3a059a96fc307f2b94631ad9dd162cc2a1276a
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-09-19 14:27:42 +0300

    Collect customer comment,details and give an alert

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,20 @@ $(document).ready(function(){
 
   });
 });
+$(document).ready(function(){
+  $("form#user").submit(function(event) {
+    event.preventDefault();
+
+    var userName=$("#name").val();
+    var email=$("#email").val();
+    var comment=$("#comment").val();
+    if(userName&&email&&comment){
+      alert("Hi "+userName +". We have received your message. Thank you for reaching out to us.");
+    
+    }
+    else{
+      alert("All fields required");
+    }
+    $("form").trigger("reset");
+  });
+});
